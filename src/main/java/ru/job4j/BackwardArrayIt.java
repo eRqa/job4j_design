d01a1efcdc2fc66e5b6883e19ac343952c12768a
eRqa <ivlasov.spb@gmail.com>
2020-12-31 19:27:04 +0300

    Итератор для массива с обратным перебором

---
+++ src/main/java/ru/job4j/BackwardArrayIt.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BackwardArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        point = data.length == 0 ? 0 : data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
