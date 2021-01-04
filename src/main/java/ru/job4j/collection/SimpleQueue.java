755cb40e8343ca3fb640b301c369403bc26ee84e
eRqa <ivlasov.spb@gmail.com>
2021-01-04 21:24:10 +0300

    Реализована очередь на двух стеках

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+
+    public T poll() {
+        if (out.getSize() == 0) {
+            int index = in.getSize();
+            for (int i = 0; i < index; i++) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+    }
+}
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -3,13 +3,20 @@ package ru.job4j.collection;
 public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public int getSize() {
+        return size;
+    }
+
     private int size = 0;
 
     public T pop() {
+        size--;
         return linked.deleteLast();
     }
 
     public void push(T value) {
         linked.add(value);
+        size++;
     }
 }
