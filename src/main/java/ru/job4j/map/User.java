5f14aa1869b2b12687d6c2a2de71ede4d660c304
eRqa <ivlasov.spb@gmail.com>
2021-01-05 22:31:00 +0300

    Использование модели User

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,11 +1,31 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
 
     private String name;
     private int children;
     private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendarVasilev = new GregorianCalendar(2017, 0 , 25);
+        Calendar calendarPetrov = new GregorianCalendar(2015, 0 , 25);
+        User Vasilev = new User("Vasilev", 1, calendarVasilev);
+        User Petrov = new User("Petrov", 4, calendarPetrov);
+
+        Map<User, Object> hashMap = new HashMap<>();
+        hashMap.put(Vasilev, "Vasilev");
+        hashMap.put(Petrov, "Petrov");
+
+        System.out.println(hashMap.toString());
+
+    }
 
 }
