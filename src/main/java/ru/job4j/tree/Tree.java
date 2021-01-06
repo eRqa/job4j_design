3297917e31e7556d4139aded66c4bb4e66b08972
eRqa <ivlasov.spb@gmail.com>
2021-01-06 22:49:26 +0300

    Реализован метод проверки дерева, является ли бинарным

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -24,6 +24,21 @@ class Tree<E> implements SimpleTree<E> {
         return rsl;
     }
 
+    public boolean isBinary() {
+        boolean rsl = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> node = data.poll();
+            if (root.children.size() > 2) {
+                rsl = false;
+                break;
+            }
+            data.addAll(node.children);
+        }
+        return rsl;
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
