58a2359dcfa01c7f56fc7afefb8dea30fff49601
eRqa <ivlasov.spb@gmail.com>
2021-01-07 21:12:55 +0300

    Исправлен метод фильтрации лога

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -8,15 +8,12 @@ public class LogFilter {
     public static List<String> filter(String file) {
         List<String> result = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            List<String> lines = new ArrayList<String>();
-            in.lines().forEach(lines::add);
 
-            for (String line
-                    : lines) {
-                String[] allLineParts = line.split(" ");
+            while (in.ready()) {
+                String[] allLineParts = in.readLine().split(" ");
                 String code = allLineParts[allLineParts.length - 2];
                 if (code.equals("404")) {
-                    result.add(line);
+                    result.add(in.readLine() + "\n");
                 }
             }
         } catch (Exception e) {
@@ -38,7 +35,7 @@ public class LogFilter {
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        log.forEach(System.out::println);
+        log.forEach(System.out::print);
         save(log, "404s.txt");
 
     }
