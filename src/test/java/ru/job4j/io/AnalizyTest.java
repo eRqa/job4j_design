bcab3887c1298927906b768a477fa99be77ab4b8
eRqa <ivlasov.spb@gmail.com>
2021-01-10 19:30:19 +0300

    Разботан тест на метод unavailable

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,11 +1,18 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void getUnavailableDiapasons() {
@@ -16,4 +23,24 @@ public class AnalizyTest {
         assertThat(act.toString(), is(expected));
     }
 
+    @Test
+    public void unavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            assertThat(in.readLine(), is("10:57:01;10:59:01"));
+            assertThat(in.readLine(), is("11:01:02;11:02:02"));
+        }
+    }
 }
