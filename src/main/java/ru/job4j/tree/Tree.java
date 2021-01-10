ad12d91863f5e07979ec138c6068e38a35711263
eRqa <ivlasov.spb@gmail.com>
2021-01-10 18:45:01 +0300

    Разработан метод посика по предикату

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -25,33 +26,26 @@ class Tree<E> implements SimpleTree<E> {
     }
 
     public boolean isBinary() {
-        boolean rsl = true;
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> node = data.poll();
-            if (root.children.size() > 2) {
-                rsl = false;
-                break;
-            }
-            data.addAll(node.children);
-        }
-        return rsl;
+        return this.findByPredicate((n) -> n.children.size() > 2).isEmpty();
     }
 
-    @Override
-    public Optional<Node<E>> findBy(E value) {
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> predicate) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (predicate.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
         return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        return this.findByPredicate((n) -> n.value.equals(value));
     }
 }
