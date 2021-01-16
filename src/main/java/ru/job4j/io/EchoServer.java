ea7d00beaed016e65be20d5ece74279dd4c6ea82
eRqa <ivlasov.spb@gmail.com>
2021-01-16 21:50:02 +0300

    Реализован простой сервер на сокетах

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            boolean serverWorks = true;
+            while (serverWorks) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                        if (str.equals("GET /?msg=Bye HTTP/1.1")) {
+                            serverWorks = false;
+                        }
+                    }
+                    if (serverWorks) {
+                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    }
+                }
+            }
+        }
+    }
+}
