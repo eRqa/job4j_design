2060766eba6ead6fcb771e5e6080fc0adfb008c4
eRqa <ivlasov.spb@gmail.com>
2021-01-17 14:36:41 +0300

    Minor fix

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -33,9 +33,6 @@ public class EchoServer {
                     }
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.write(message.getBytes());
-                    if (!serverWorks) {
-                        server.close();
-                    }
                 }
             }
         } catch (IOException e) {
