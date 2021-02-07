c318b8851a56fa35b83c88e20209714b653b502a
eRqa <ivlasov.spb@gmail.com>
2021-02-07 20:06:55 +0300

    Создан класс и описан пример его XML представления

---
+++ src/main/java/ru/job4j/io/serialization/Flat.java
@@ -0,0 +1,13 @@
+package ru.job4j.io.serialization;
+
+import ru.job4j.io.Engine;
+
+public class Flat {
+
+    private boolean itIsFirstFloor;
+    private int square;
+    private String ownerName;
+    private String[] rooms;
+    private Engine someObject;
+
+}
