4777586ef2bd83e1403ee6df004ddad2c27dd6f4
eRqa <ivlasov.spb@gmail.com>
2021-02-09 23:47:41 +0300

    Реализован класс для подключения к БД. Реализован класс загрузки app properties

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,33 @@
+package ru.job4j.jdbc;
+
+import jdk.jfr.EventSettings;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws Exception {
+
+        Settings settings = new Settings();
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream inputStream = loader.getResourceAsStream("app.properties")) {
+            settings.load(inputStream);
+        }
+        String url = settings.getValue("hibernate.connection.url");
+        String login = settings.getValue("hibernate.connection.username");
+        String password = settings.getValue("hibernate.connection.password");
+
+        Class.forName("org.postgresql.Driver");
+//        String url = "jdbc:postgresql://localhost:5432/job4j_database";
+//        String login = "postgres";
+//        String password = "password";
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
