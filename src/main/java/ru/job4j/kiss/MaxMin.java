1998bc2560bc3038b4811f71d19391935c2e539f
eRqa <ivlasov.spb@gmail.com>
2021-04-01 20:52:15 +0300

    Принцип KISS

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -6,11 +6,11 @@ import java.util.function.Predicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return findBy(value, comparator, (v) -> v == -1);
+        return findBy(value, comparator, (v) -> v < 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return findBy(value, comparator, (v) -> v == 1);
+        return findBy(value, comparator, (v) -> v > 0);
     }
 
     private <T> T findBy(List<T> value, Comparator<T> comparator,
